0e7348c96085ea0439519919dd045e659c21b5f4
viper-7 <viper7@viper-7.com>
2015-02-19 15:36:50 +1100

    Changed nickname

--- startBot.php
+++ startBot.php
@@ -5,4 +5,4 @@ ftbbot::$db_file = __DIR__ . '/ftb_triggers.sqlite';
 ftbbot::$log_file = __DIR__ . '/ftb_log.sqlite';
 ftbbot::$backup_path = __DIR__ . '/backup_triggers.sqlite';
 
-startBot('irc.esper.net', 6667, 'FTB-Bot', Array('#ftb'), TRUE);
+startBot('irc.esper.net', 6667, 'Druss', Array('#ftb'), TRUE);
