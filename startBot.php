3d7e3b6a1719fd58f36bc27b304f2c3b3ba05708
viper-7 <viper7@viper-7.com>
2015-02-19 15:40:26 +1100

    derp

--- startBot.php
+++ startBot.php
@@ -1,8 +1,8 @@
 <?php
 include 'lib/Bootstrap.php';
 
-ftbbot::$db_file = __DIR__ . '/ftb_triggers.sqlite';
-ftbbot::$log_file = __DIR__ . '/ftb_log.sqlite';
-ftbbot::$backup_path = __DIR__ . '/backup_triggers.sqlite';
+ftb::$db_file = __DIR__ . '/ftb_triggers.sqlite';
+ftb::$log_file = __DIR__ . '/ftb_log.sqlite';
+ftb::$backup_path = __DIR__ . '/backup_triggers.sqlite';
 
 startBot('irc.esper.net', 6667, 'Druss', Array('#ftb'), TRUE);
